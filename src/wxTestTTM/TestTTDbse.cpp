bf05c2515d5320808ed41c724a836125e92641da
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2020-11-18 17:05:40 +0000

    Compute initial working directory from master database
    git-svn-id: https://localhost/svn/wxTTM/trunk@7206 7754d8ab-9b54-e011-a251-0001c006bdc7

--- src/wxTestTTM/TestTTDbse.cpp
+++ src/wxTestTTM/TestTTDbse.cpp
@@ -2,6 +2,11 @@
 #include "CppUnitTest.h"
 
 #include "TTDbse.h"
+#include "DriverManager.h"
+#include "Statement.h"
+#include "ResultSet.h"
+#include "SQLException.h"
+#include "InfoSystem.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,8 +15,33 @@ namespace wxTestTTM
   // Rune once before all tests
   TEST_MODULE_INITIALIZE(SetUpTestTTM)
   {
-    // TODO: Somehow configurable
-    wxSetWorkingDirectory("..\\..\\..\\..\\..\\TTM");
+    wxString masterStr = "DRIVER=SQL Server;SERVER=localhost;Trusted_Connection=Yes;";
+    wxString sql = 
+      "SELECT sys.master_files.physical_name "
+      "  FROM sys.databases INNER JOIN sys.master_files ON sys.databases.database_id = sys.master_files.database_id "
+      " WHERE sys.databases.name = 'TTM_UTEST' AND type_desc = 'ROWS'"
+    ;
+
+    Connection *masterConnection = 0;
+    Statement  *stmtPtr = 0;
+    ResultSet  *resPtr = 0;
+  
+    masterConnection = DriverManager::instance()->GetConnection(masterStr, "", "");
+
+    Assert::IsNotNull(masterConnection);
+    
+    stmtPtr = masterConnection->CreateStatement();
+    resPtr = stmtPtr->ExecuteQuery(sql);
+  
+    wxChar  tmp[100];
+    Assert::IsTrue(resPtr != NULL && resPtr->Next() && resPtr->GetData(1, tmp, 100), L"Could not find tournament / database \"TTM_UTEST\", please create it before running the tests");
+    
+    delete resPtr;
+    delete stmtPtr;
+    delete masterConnection;
+
+    wxString cd = wxFileName(wxFileName(tmp).GetPath()).GetPath();
+    wxSetWorkingDirectory(cd);
 
     wxString connStr = "DRIVER=SQL Server;SERVER=localhost;Trusted_Connection=Yes;DATABASE=TTM_UTEST;AnsiNPW=No;";
     bool ret = TTDbse::instance()->OpenDatabase(connStr, false);
