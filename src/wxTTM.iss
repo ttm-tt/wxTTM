3651741ee36b8b1ea4e5c15cabc6e8f5ba8203ff
christoph <christoph@7754d8ab-9b54-e011-a251-0001c006bdc7>
2022-07-05 13:24:42 +0000

    v22.06.02
    git-svn-id: https://localhost/svn/wxTTM/trunk@8434 7754d8ab-9b54-e011-a251-0001c006bdc7

--- src/wxTTM.iss
+++ src/wxTTM.iss
@@ -5,7 +5,7 @@
 
 [Setup]
 AppName=TTM
-AppVerName=TTM 22.06.01
+AppVerName=TTM 22.06.02
 AppPublisher=Christoph Theis
 AppMutex=TTM
 DefaultDirName={autopf}\TTM
--- src/wxTTM/wx/TT32App.cpp
+++ src/wxTTM/wx/TT32App.cpp
@@ -35,7 +35,7 @@ static int defaultType  = TT_REGULAR;
 static int defaultTable = TT_ITTF;
 
 // Muss hier stehen, weil es sonst nicht compiliert
-static const wxString versionNumber = "22.06.01";
+static const wxString versionNumber = "22.06.02";
 static const wxString version = "Version " + versionNumber;
 
 static wxString licensee = " Christoph Theis";
